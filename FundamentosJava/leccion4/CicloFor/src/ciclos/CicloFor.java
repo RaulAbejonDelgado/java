20262b07b42c5581037e3a4f55fe79019986b90f
raul.abejon.delgado <raul.abejon.delgado@gmail.com>
2018-08-02 19:32:16 +0200

    leccion 4 Ciclo for

---
+++ FundamentosJava/leccion4/CicloFor/src/ciclos/CicloFor.java
@@ -0,0 +1,19 @@
+package ciclos;
+
+/**
+ *
+ * 
+ */
+public class CicloFor {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        int limite = 10;
+        for (int contador = 0; contador < limite; contador++) {
+            System.out.println("contador = " + contador);
+            }
+        }
+    
+    }
