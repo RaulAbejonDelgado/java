ca2fe2fc7d4caf8318499052040ff4d8afdda52f
raul.abejon.delgado <raul.abejon.delgado@gmail.com>
2018-08-03 00:15:49 +0200

    leccion 7 Paso por referencia

---
+++ FundamentosJava/leccion7/PasoPorReferencia/src/pasoporreferencia/Persona.java
@@ -0,0 +1,26 @@
+package pasoporreferencia;
+
+public class Persona {
+
+   //Atributos de una clase
+    //No es necesario asignar valores
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+
+    //Metodos de la clase
+    //Los usaran los objetos de esta clase
+    public void desplegarNombre() {
+        System.out.println("Nombre : " + nombre);
+        System.out.println("Apellido Paterno : " + apellidoPaterno);
+        System.out.println("Apellido Materno : " + apellidoMaterno);
+    }
+
+    void cambiarNombre(String nombre) {
+        this.nombre = nombre; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String obtenerNombre() {
+        return this.nombre;
+    }
+}
