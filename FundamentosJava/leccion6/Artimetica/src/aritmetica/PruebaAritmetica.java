bef537c92b12a9a78fa1f7611649d9bad6a8cfba
raul.abejon.delgado <raul.abejon.delgado@gmail.com>
2018-08-02 21:42:33 +0200

    leccion 6 Metodos

---
+++ FundamentosJava/leccion6/Artimetica/src/aritmetica/PruebaAritmetica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmetica;
+
+/**
+ *
+ * @author drohne
+ */
+public class PruebaAritmetica {
+    public static void main(String args[]){
+        
+        //Creamos un objeto de la clase Aritmetica
+        Aritmetica a = new Aritmetica();
+        //Llamamos el metodo sumar y recibimos el valor devuelto
+        int resultado = a.sumar(5, 3);
+        System.out.println("Resultado:" + resultado);
+    }
+}
