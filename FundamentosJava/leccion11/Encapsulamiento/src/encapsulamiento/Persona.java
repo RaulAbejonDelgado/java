d43b5fe5dc16b21476749db0a8dd14306fdc19b4
raul.abejon.delgado <raul.abejon.delgado@gmail.com>
2018-08-03 18:04:07 +0200

    leccion 11 Encapsulamiento

---
+++ FundamentosJava/leccion11/Encapsulamiento/src/encapsulamiento/Persona.java
@@ -0,0 +1,51 @@
+package encapsulamiento;
+
+public class Persona {
+    
+    //Atributos privados
+    private String nombre;
+    private String apellido;
+    private boolean borrado;
+    
+    //Constructor Vacio
+    public Persona() {
+    }
+    //Constructor con 3 argumentos
+    public Persona(String nombre, String apellido, boolean borrado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.borrado = borrado;
+    }
+
+
+    //Métodos publicos para acceder y/o modificar los atributos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(boolean borrado) {
+        this.borrado = borrado;
+    }
+    
+    //Método que imprime el estado del objeto
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", borrado=" + borrado + '}';
+    }
+}
