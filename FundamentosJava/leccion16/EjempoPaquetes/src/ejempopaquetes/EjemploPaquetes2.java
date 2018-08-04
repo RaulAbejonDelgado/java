9d7c3a19ac6e090f0f60fe4a280564e109ebd6ff
raul.abejon.delgado <raul.abejon.delgado@gmail.com>
2018-08-04 12:56:45 +0200

    leccion 16 Paquetes package

---
+++ FundamentosJava/leccion16/EjempoPaquetes/src/ejempopaquetes/EjemploPaquetes2.java
@@ -0,0 +1,11 @@
+package ejempopaquetes;
+
+//Importamos el metodo estatico a utilizar
+import static drohne.paqueteuno.Consumibles.imprimir;
+
+public class EjemploPaquetes2 {
+
+    public static void main(String[] args) {
+        imprimir("Hola");
+    }   
+}
---
+++ FundamentosJava/leccion16/EjempoPaquetes/src/ejempopaquetes/EjempoPaquetes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejempopaquetes;
+import drohne.paqueteuno.Consumibles;
+
+/**
+ *
+ * @author drohne
+ */
+public class EjempoPaquetes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //hacemos uso del metodo imprimir de la clase Consumibles ya que lo
+        //hemos importado al comienzo del archivo
+        Consumibles.imprimir("Raul");
+    }
+    
+}
