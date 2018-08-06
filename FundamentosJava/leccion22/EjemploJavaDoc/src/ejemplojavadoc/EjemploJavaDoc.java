d419ec9ef4091e617e054a73532f31eefa8e59fb
raul.abejon.delgado <raul.abejon.delgado@gmail.com>
2018-08-06 17:26:32 +0200

    leccion 22 JavaDoc

---
+++ FundamentosJava/leccion22/EjemploJavaDoc/src/ejemplojavadoc/EjemploJavaDoc.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplojavadoc;
+
+/**
+ * Clase para probar el concepto de JavaDoc
+ * @author RaulAbejon Delgado
+ * @version 1.0
+ */
+public class EjemploJavaDoc {
+
+    /**
+     * Metodo que ejecuta la prueba de la clase Aritmetica
+     * @param args argumentos de la linea de comandos
+     */
+    public static void main(String[] args) {
+        int resultado = new Aritmetica(3, 2).sumar();
+
+        System.out.println("resultado:" + resultado);
+    }
+}
