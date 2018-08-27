32b5e8f60bfa91cc6e11fe2be7fcffc59429467b
raul.abejon.delgado <raul.abejon.delgado@gmail.com>
2018-08-27 18:24:33 +0200

    leccion 29 InstanceOf

---
+++ FundamentosJava/leccion29/EjemploInsteceOf/src/ejemploinsteceof/Elipse.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploinsteceof;
+
+/**
+ *
+ * @author drohne
+ */
+public class Elipse extends Circulo{
+    @Override
+    public void dibujar(){
+        System.out.println("dibujar Elipse");
+    }
+}
