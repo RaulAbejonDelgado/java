2a8e43b3a7511164651ba036d0b033330f84d88c
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2019-11-10 19:33:52 +0100

    config properties server

---
+++ micro-servicios/springboot-service-config/src/test/java/com/raul/spring/microservices/config/SpringbootServiceConfigApplicationTests.java
@@ -0,0 +1,12 @@
+package com.raul.spring.microservices.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceConfigApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
--- micro-servicios/springboot-service-item/src/main/java/com/raul/spring/microservices/item/controllers/ItemController.java
+++ micro-servicios/springboot-service-item/src/main/java/com/raul/spring/microservices/item/controllers/ItemController.java
@@ -1,21 +1,38 @@
 package com.raul.spring.microservices.item.controllers;
 
+import com.netflix.discovery.converters.Auto;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.raul.spring.microservices.item.models.entity.Item;
 import com.raul.spring.microservices.item.models.entity.Product;
 import com.raul.spring.microservices.item.models.service.ItemsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ItemController {
 
+    private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private ItemsService itemsService;
+
+    @Value("${configuration.text}")
+    private String text;
 
     @GetMapping("/list")
     public List<Item> list(){
@@ -47,6 +64,22 @@ public class ItemController {
 
         return i ;
 
+    }
+
+    @GetMapping("/get-config")
+    public ResponseEntity<?> getConfig(@Value("${server.port}") String port){
+
+        log.info("The input value of getConfig() is -> "+ port);
+
+        Map<String,String> json = new HashMap<>();
+        json.put("text", text);
+        json.put("port", port);
+
+        if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")){
+            json.put("configuration.author.name",env.getProperty("configuration.author.name"));
+            json.put("configuration.author.email",env.getProperty("configuration.author.email"));
+        }
+        return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
     }
 
 
