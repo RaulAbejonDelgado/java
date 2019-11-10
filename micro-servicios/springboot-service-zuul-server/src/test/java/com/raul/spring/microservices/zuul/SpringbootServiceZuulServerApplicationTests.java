dfb01e19e6ca37cc3feeef376899abb446af9bcf
rabejon <rabejon@kernet.es>
2019-11-10 13:15:00 +0100

    zuul server api gateway

---
+++ micro-servicios/springboot-service-zuul-server/src/test/java/com/raul/spring/microservices/zuul/SpringbootServiceZuulServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.raul.spring.microservices.zuul;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceZuulServerApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
