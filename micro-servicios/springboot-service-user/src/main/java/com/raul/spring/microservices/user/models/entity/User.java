d47b1268e485c24aab1e8d175a9a99cce6701843
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2019-11-12 09:22:51 +0100

    User service for auth

---
+++ micro-servicios/springboot-service-user/src/main/java/com/raul/spring/microservices/user/models/entity/User.java
@@ -0,0 +1,96 @@
+package com.raul.spring.microservices.user.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String username;
+
+    @Column(unique = true, length = 60)
+    private String password;
+    private Boolean enabled;
+    private String name;
+    private String surname;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(inverseJoinColumns = @JoinColumn(name="role_id") ,
+            uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role_id"})})
+    List<Role> roles;
+
+    @Column(unique = true, length = 100)
+    private String email;
+
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ micro-servicios/springboot-service-user/src/test/java/com/raul/spring/microservices/user/SpringbootServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.spring.microservices.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
