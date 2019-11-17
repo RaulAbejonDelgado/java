b7a098c3830fcbeb38aa3e4e60fe26fd7248bc3d
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2019-11-17 19:15:34 +0100

    Adding postgresql and config from config-server

--- micro-servicios/springboot-service-user/src/main/resources/import.sql
+++ micro-servicios/springboot-service-user/src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (username,password,enabled,name,email,surname) VALUES('drohne','123',1,'drohne','drohne@gmail.com','xxxx')
-INSERT INTO users (username,password,enabled,name,email,surname) VALUES('drohne2','1233',1,'drohne2','drohne2@gmail.com','xxxx')
+INSERT INTO users (username,password,enabled,name,email,surname,logging_tries) VALUES('drohne','$2a$10$r1ENt2ARboU7ltAv5lJtTuzeEmesRiOU5sYgGb66h0sNw3eNTUb5W',true,'drohne','drohne@gmail.com','xxxx',0)
+INSERT INTO users (username,password,enabled,name,email,surname,logging_tries) VALUES('rultest','$2a$10$r1ENt2ARboU7ltAv5lJtTuzeEmesRiOU5sYgGb66h0sNw3eNTUb5W',true,'rultest','rultest@gmail.com','xxxx',0)
 
 INSERT INTO roles (name) VALUES ('ROLE_USER')
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN')
 
-INSERT INTO `users_roles` (user_id,role_id) VALUES(1,2)
-INSERT INTO `users_roles` (user_id,role_id) VALUES(2,1)
+INSERT INTO users_roles (user_id,role_id) VALUES(1,2)
+INSERT INTO users_roles (user_id,role_id) VALUES(2,1)
