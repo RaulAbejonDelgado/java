8788844d7bb5f10b86fb42c5dbabeefa1b70c8ee
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2020-02-16 11:08:51 +0100

    Added to permitAll Locale controller

--- spring-jpa-v1/src/main/java/com/raul/spring/jpa/springjpav1/SpringSecurityConfig.java
+++ spring-jpa-v1/src/main/java/com/raul/spring/jpa/springjpav1/SpringSecurityConfig.java
@@ -3,17 +3,11 @@ package com.raul.spring.jpa.springjpav1;
 import com.raul.spring.jpa.springjpav1.auth.handler.LoginSuccessHandler;
 import com.raul.spring.jpa.springjpav1.models.service.UserDetailsJpaServcie;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -46,7 +40,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/","/css/**","/js/**","/images/**","/list/**").permitAll()
+                .antMatchers("/","/css/**","/js/**","/images/**","/list/**","/locale/**").permitAll()
                 .antMatchers("/show/**","/uploads/**").hasAnyRole("USER")
                 .antMatchers("/form/**","/delete/**","/saleorder/**").hasAnyRole("ADMIN")
                 .anyRequest().authenticated()
