ff836984655aaa2b5a13548ce3b42af94c33dea5
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2020-02-16 09:41:36 +0100

    Spring i18n without locale controller losing the paging when lenguage is changed

--- spring-jpa-v1/src/main/java/com/raul/spring/jpa/springjpav1/MvcConfig.java
+++ spring-jpa-v1/src/main/java/com/raul/spring/jpa/springjpav1/MvcConfig.java
@@ -5,11 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.nio.file.Paths;
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -35,5 +40,39 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/error_http").setViewName("error_http");
+    }
+
+    /*-----------------------------LOCALE--i18N-------------------------------*/
+
+    /**
+     * BEAN localeresolver will managed the adapter where it is going to saved
+     * the locale in httpsession or in a cookie.
+      *
+     */
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("es","ES"));
+        return localeResolver;
+    }
+
+    /**
+     * properties files that had translataions
+     * Interceptor -> LocaleChangeInterceptor will active on http request
+     * when lang exist how requestparam e.g '?lang="es_ES"' this interceptor will do
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    /**
+     * To add localeChangeInterceptor bean to spring
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
